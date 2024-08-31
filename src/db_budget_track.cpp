73339ed940bf8c17fd60189c993a327ad449fc48
Guy Wilson <wilson.guy@gmail.com>
2024-08-31 10:04:01 +0100

    Added DBBudgetTrack

---
+++ src/db_budget_track.cpp
@@ -0,0 +1,44 @@
+#include <string>
+
+#include "logger.h"
+#include "pfm_error.h"
+#include "db_budget_track.h"
+#include "db_base.h"
+#include "db.h"
+#include "strdate.h"
+
+using namespace std;
+
+DBResult<DBBudgetTrack> DBBudgetTrack::retrieveByBudgetId(pfm_id_t budgetId) {
+    char szStatement[SQL_STATEMENT_BUFFER_LEN];
+    DBResult<DBBudgetTrack> result;
+
+    snprintf(
+        szStatement, 
+        SQL_STATEMENT_BUFFER_LEN, 
+        sqlSelectByBudgetId, 
+        budgetId);
+
+    result.retrieve(szStatement);
+
+    return result;
+}
+
+int DBBudgetTrack::retrieveLatestByBudgetId(pfm_id_t budgetId) {
+    char szStatement[SQL_STATEMENT_BUFFER_LEN];
+    DBResult<DBBudgetTrack> result;
+
+    snprintf(
+        szStatement, 
+        SQL_STATEMENT_BUFFER_LEN, 
+        sqlSelectLatestByBudgetId, 
+        budgetId);
+
+    int rowsRetrievedCount = result.retrieve(szStatement);
+
+    if (rowsRetrievedCount == 1) {
+        set(result.getResultAt(0));
+    }
+
+    return rowsRetrievedCount;
+}
---
+++ src/db_budget_track.h
@@ -0,0 +1,170 @@
+#include <string>
+#include <string.h>
+#include <stdint.h>
+
+#include "pfm_error.h"
+#include "db.h"
+#include "db_base.h"
+#include "strdate.h"
+#include "money.h"
+
+using namespace std;
+
+#ifndef __INCL_BUDGET_TRACK
+#define __INCL_BUDGET_TRACK
+
+class DBBudgetTrack : public DBEntity {
+    private:
+        const char * sqlSelectByBudgetId = 
+            "SELECT " \
+            "id," \
+            "budget_id," \
+            "date," \
+            "balance," \
+            "created," \
+            "updated " \
+            "FROM period_budget_track " \
+            "WHERE budget_id = %lld;";
+
+        const char * sqlSelectLatestByBudgetId = 
+            "SELECT " \
+            "id," \
+            "budget_id," \
+            "date," \
+            "balance," \
+            "created," \
+            "updated " \
+            "FROM period_budget_track " \
+            "WHERE budget_id = %lld " \
+            "ORDER BY date DESC " \
+            "LIMIT 1;";
+
+        const char * sqlInsert = 
+            "INSERT INTO period_budget_track (" \
+            "budget_id," \
+            "date," \
+            "balance," \
+            "created," \
+            "updated)" \
+            "VALUES (" \
+            "%lld, '%s', %.2f, '%s', '%s');";
+
+        const char * sqlUpdate = 
+            "UPDATE period_budget_track SET " \
+            "budget_id = %lld," \
+            "date = '%s'," \
+            "balance = %.2f," \
+            "updated = '%s' " \
+            "WHERE id = %lld;";
+
+        const char * sqlDelete = 
+            "DELETE FROM period_budget_track WHERE id = %lld;";
+
+    public:
+        pfm_id_t budgetId;
+        StrDate date;
+        Money balance;
+
+        DBBudgetTrack() : DBEntity() {
+            clear();
+        }
+
+        DBBudgetTrack(const DBBudgetTrack & src) : DBEntity(src) {
+            set(src);
+        }
+
+        void clear() {
+            DBEntity::clear();
+
+            this->budgetId = 0;
+            this->date.clear();
+            this->balance = 0.0;
+        }
+
+        void set(const DBBudgetTrack & src) {
+            DBEntity::set(src);
+
+            this->budgetId = src.budgetId;
+            this->date = src.date;
+            this->balance = src.balance;
+        }
+
+        void print() {
+            DBEntity::print();
+
+            cout << "BudgetId: " << budgetId << endl;
+            cout << "Date: '" << date.shortDate() << "'" << endl;
+            cout << "Balance: " << balance.getRawStringValue() << endl;
+        }
+
+        void assignColumn(DBColumn & column) override {
+            DBEntity::assignColumn(column);
+            
+            if (column.getName() == "budget_id") {
+                budgetId = column.getIDValue();
+            }
+            else if (column.getName() == "date") {
+                date = column.getValue();
+            }
+            else if (column.getName() == "balance") {
+                balance = column.getDoubleValue();
+            }
+        }
+
+        const char * getTableName() override {
+            return "period_budget_track";
+        }
+
+        const char * getInsertStatement() override {
+            static char szStatement[SQL_STATEMENT_BUFFER_LEN];
+
+            string now = StrDate::getTimestamp();
+
+            snprintf(
+                szStatement, 
+                SQL_STATEMENT_BUFFER_LEN,
+                sqlInsert,
+                budgetId,
+                date.shortDate().c_str(),
+                balance.getDoubleValue(),
+                now.c_str(),
+                now.c_str());
+
+            return szStatement;
+        }
+
+        const char * getUpdateStatement() override {
+            static char szStatement[SQL_STATEMENT_BUFFER_LEN];
+
+            string now = StrDate::getTimestamp();
+
+            snprintf(
+                szStatement, 
+                SQL_STATEMENT_BUFFER_LEN,
+                sqlUpdate,
+                budgetId,
+                date.shortDate().c_str(),
+                balance.getDoubleValue(),
+                now.c_str(),
+                id);
+
+            return szStatement;
+        }
+
+        const char * getDeleteStatement() override {
+            static char szStatement[SQL_STATEMENT_BUFFER_LEN];
+
+            snprintf(
+                szStatement, 
+                SQL_STATEMENT_BUFFER_LEN,
+                sqlDelete,
+                id);
+
+            return szStatement;
+        }
+
+        DBResult<DBBudgetTrack> retrieveByBudgetId(pfm_id_t budgetId);
+        int retrieveLatestByBudgetId(pfm_id_t budgetId);
+};
+
+#endif
