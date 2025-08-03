ec8903f4881bc76beb2e74364bcbee9f65b15c52
Guy Wilson <wilson.guy@gmail.com>
2025-08-03 10:11:22 +0100

    Updated version

--- src/version.c
+++ src/version.c
@@ -1,7 +1,7 @@
 #include "version.h"
 
-#define __BDATE__      "2025-08-02 17:59:20"
-#define __BVERSION__   "1.7.002"
+#define __BDATE__      "2025-08-03 10:11:01"
+#define __BVERSION__   "1.7.003"
 
 const char * getVersion(void) {
     return __BVERSION__;
