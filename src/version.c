fd6764872db74465ceeeb037b8e3d700a266d406
Guy Wilson <wilson.guy@gmail.com>
2025-09-13 21:21:52 +0100

    Set default line length to 384 chars

--- src/version.c
+++ src/version.c
@@ -1,7 +1,7 @@
 #include "version.h"
 
-#define __BDATE__      "2025-09-13 14:16:32"
-#define __BVERSION__   "1.8.035"
+#define __BDATE__      "2025-09-13 21:20:46"
+#define __BVERSION__   "1.8.036"
 
 const char * getVersion(void) {
     return __BVERSION__;
