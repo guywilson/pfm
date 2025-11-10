f489545307ae647be548c24512a37539cddd3d73
Guy Wilson <wilson.guy@gmail.com>
2025-11-10 15:40:09 +0000

    Fix compiler warning

--- src/version.c
+++ src/version.c
@@ -1,7 +1,7 @@
 #include "version.h"
 
-#define __BDATE__      "2025-11-10 15:31:07"
-#define __BVERSION__   "2.0.004"
+#define __BDATE__      "2025-11-10 15:39:54"
+#define __BVERSION__   "2.0.005"
 
 const char * getVersion(void) {
     return __BVERSION__;
