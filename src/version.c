5b53c739e8555633c70b923388b1ae4af931b123
Guy Wilson <wilson.guy@gmail.com>
2026-01-03 22:45:02 +0000

    Address bug in put_money on Ubuntu

--- src/version.c
+++ src/version.c
@@ -1,7 +1,7 @@
 #include "version.h"
 
-#define __BDATE__      "2026-01-03 16:04:43"
-#define __BVERSION__   "2.1.030"
+#define __BDATE__      "2026-01-03 22:44:06"
+#define __BVERSION__   "2.1.031"
 
 const char * getVersion(void) {
     return __BVERSION__;
